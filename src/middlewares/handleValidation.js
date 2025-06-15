b11112b876476f5eb21c24d63e81e687ba57a9d3
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-06-15 20:01:13 -0300

    .

--- src/middlewares/handleValidation.js
+++ src/middlewares/handleValidation.js
@@ -4,6 +4,7 @@ const handleValidation = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    
     const extractedErrors = errors.array().map(err => ({ [err.param]: err.msg }));
     return res.status(422).json({ errors: extractedErrors });
   }
